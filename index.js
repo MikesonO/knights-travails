10f31685a6e4fe783921347f091b154c9041a956
mikeson <mikeson.jnr@gmail.com>
2023-01-07 14:24:17 +0000

    Complete tests

---
+++ index.js
@@ -0,0 +1,24 @@
+import knightMoves from "./knightMoves.js"; 
+
+// Test
+
+knightMoves([3, 3], [4, 3]);
+
+/*
+  Output:
+  => You made it in 3 moves!  Here's your path:
+  [ 3, 3 ]
+  [ 5, 2 ]
+  [ 3, 1 ]
+  [ 4, 3 ] 
+*/
+
+knightMoves([4, 5], [2, 1]);
+
+/*
+  Output:
+  => You made it in 2 moves!  Here's your path:
+  [ 4, 5 ]
+  [ 3, 3 ]
+  [ 2, 1 ]
+*/
